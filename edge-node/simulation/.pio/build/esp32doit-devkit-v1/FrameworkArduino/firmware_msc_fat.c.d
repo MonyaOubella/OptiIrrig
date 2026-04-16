6450cab89c947e9a47bc241a66cfc3dd0b1fc061
EVL44 <spoope12345@gmail.com>
2026-04-16 16:06:54 +0100

    EVL44: interactive dashboard

---
+++ edge-node/simulation/.pio/build/esp32doit-devkit-v1/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32doit-devkit-v1/FrameworkArduino/firmware_msc_fat.c.o: \
+ C:/Users/ACER\ NITRO\ 5/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ C:/Users/ACER\ NITRO\ 5/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
